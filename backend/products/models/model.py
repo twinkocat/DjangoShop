72bd74776bbda7aab103ab1aa41d3e14af961f8b
twinkocat <msbulahmaxim@gmail.com>
2022-06-20 23:30:30 +0300

    delete field old_price and delete few 2 model methods

--- backend/products/models/model.py
+++ backend/products/models/model.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-from products.services import format_price, format_old_price
+from products.services import format_price
 
 
 class Model(models.Model):
     name = models.CharField(max_length=100, blank=False, verbose_name="Модель")
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
-    old_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     count = models.PositiveIntegerField(verbose_name="Количество на складе")
     description = models.TextField(blank=False, verbose_name="Описание", default='description')
 
@@ -20,9 +19,4 @@ class Model(models.Model):
     def get_price_display(self):
         return format_price(self.price)
 
-    def get_old_price_display(self):
-        return format_price(self.old_price)
-
-    def get_formatted_price_display(self):
-        return format_old_price(self.old_price, self.price)
 
