1bfd0b097c313363f792ec84d4f4044b9ee38a2a
twinkocat <msbulahmaxim@gmail.com>
2022-06-20 23:31:15 +0300

    refactor and delete unusual service

--- backend/products/services.py
+++ backend/products/services.py
@@ -3,27 +3,9 @@ from decimal import Decimal
 
 
 def format_price(price: Decimal) -> str:
-    """Временно взята логика с
-     https://github.com/tough-dev-school/education-backend/tree/master/src/products
-                                                            спасибо :3
-     """
+    """Формат валюты"""
     if price is not None and price:
-        return format_decimal(price, locale='ru_RU')
-
-    return '0'
-
-
-def format_old_price(old_price: Decimal, new_price: Decimal) -> str:
-    formatted_new_price = format_price(new_price)
-    formatted_old_price = format_price(old_price)
-
-    if formatted_old_price == '0':
-        return f'{formatted_new_price} UAH'
-
-    formatted_old_price = ''.join([f'{digit}\u0336' for digit in formatted_old_price])
-    formatted_old_price = formatted_old_price.replace('\xa0\u0336', '')
-
-    return f'{formatted_old_price} {formatted_new_price} UAH'
+        return f'{price} UAH'
 
 
 def get_discount(discount, price: Decimal) -> dict:
@@ -32,7 +14,7 @@ def get_discount(discount, price: Decimal) -> dict:
         discount_price = price - (price * discount.value)
         return {
             'discount_name': discount.name,
-            'discount_value': f'{discount.value * 100}%',
+            'discount_value': f'{discount.value * 100} %',
             'discount_price': f'{discount_price} UAH'
         }
 
