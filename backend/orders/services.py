103ebcb8e3c232bae1280e240475b459562f3f5e
twinkocat <msbulahmaxim@gmail.com>
2022-06-28 13:07:53 +0300

    add service for calculate total price order

--- backend/orders/services.py
+++ backend/orders/services.py
@@ -2,6 +2,20 @@ from django.db.models import Avg
 
 
 def calculate_price(product):
-    price = product.model.price
-    return str(price)
+    """calculate order price for show current user total price buying products"""
+    price_value = product.values_list('model__price', flat=True)
+    discount_value = product.values_list('discount__value', flat=True)
+
+    total_price = []
+    for price, discount in zip(price_value, discount_value):
+        if discount is not None:
+            total_price.append(price - price * discount)
+        if discount is None:
+            total_price.append(price)
+
+    return sum(total_price)
+
+
+
+
 
