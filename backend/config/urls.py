392e240a06b731ec01e6d37257ab97df0e398cff
twinkocat <msbulahmaxim@gmail.com>
2022-06-28 13:11:04 +0300

    little folders changes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
-from products.api.viewsets import ProductsListViewSet
+from products.api.view import ProductsListViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductsListViewSet)
